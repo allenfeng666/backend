39cdd58d5d722e27069a490b589affcb387be66b
allenfeng666 <allenfeng666@gmail.com>
2023-07-14 11:00:10 -0700

    update ChatGPT API connection for backend

--- server.js
+++ server.js
@@ -14,85 +14,68 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+const axios_1 = __importDefault(require("axios"));
 const webScraper_1 = require("./webScraper");
+const dotenv_1 = __importDefault(require("dotenv"));
 const app = (0, express_1.default)();
 const port = 3000;
-app.use(express_1.default.json());
 app.use((0, cors_1.default)({
     origin: ['http://localhost:3001'],
 }));
+app.use(express_1.default.json());
+// Load the environment variables from .env file
+dotenv_1.default.config();
 // Constants for ChatGPT API
 const API_URL = 'https://api.openai.com/v1/chat/completions';
-const API_KEY = 'YOUR_API_KEY';
-app.post('/summarize_url', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const API_KEY = process.env.API_KEY;
+app.post('/summarize_url', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const { url, languages } = req.body;
+        const { url, length, language } = req.body;
+        // Input validation
+        if (!url || !length || !language) {
+            return res.status(400).json({ error: 'Missing required fields' });
+        }
         // Perform web scraping to extract relevant data from the webpage
         const extractedData = yield (0, webScraper_1.scrapeData)(url);
-        // Perform translation using the selected translation service
-        const translatedData = yield translateData(extractedData, languages);
-        // Generate customized summaries for each language
-        const summaries = yield generateSummaries(url, languages, translatedData, extractedData);
-        res.status(200).json({ summaries });
+        // Generate the prompt for the ChatGPT API
+        const prompt = `The HTML page below is the static source code of the website. 
+    You need to summarize the content of the website to provide an overview of what 
+    the website does, how useful it is to the user, and some competitors of similar 
+    websites. The summarization should be translated into ${language}, and around 
+    ${length} words.\n
+    HTML Content:\n
+    ${extractedData.htmlContent}
+    `;
+        // Send the request to the ChatGPT API
+        const response = yield axios_1.default.post(API_URL, {
+            prompt,
+        }, {
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${API_KEY}`,
+            },
+        });
+        // Extract the summaries from the response
+        const summaries = response.data.choices[0].text.trim();
+        // Return the summaries and other data
+        res.status(200).json({ summaries, url, extractedData });
     }
     catch (error) {
         console.error('Error:', error);
-        res.status(500).json({ error: 'An error occurred' });
+        next(error);
     }
 }));
-// Simulated function for translation
-function translateData(data, languages) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            // Simulate translation using an external translation service or library
-            // Implement your translation logic here
-            // ...
-            // Return the translated data
-            return Object.assign(Object.assign({}, data), { translatedContent: 'Translated Content' });
-        }
-        catch (error) {
-            console.error('Error translating data:', error);
-            throw error;
-        }
-    });
-}
-// Simulated function for generating summaries
-function generateSummaries(url, languages, translatedData, extractedData) {
-    return __awaiter(this, void 0, void 0, function* () {
-        try {
-            // Prepare input for the ChatGPT API
-            const prompt = `URL: ${url}\n\nExtracted Data:\n${JSON.stringify(extractedData, null, 2)}\n\n`;
-            const maxTokens = 50; // Adjust the desired maximum number of tokens in the response
-            // Send the request to the ChatGPT API
-            // const response = await axios.post(
-            //   API_URL,
-            //   {
-            //     prompt,
-            //     max_tokens: maxTokens,
-            //   },
-            //   {
-            //     headers: {
-            //       'Content-Type': 'application/json',
-            //       Authorization: `Bearer ${API_KEY}`,
-            //     },
-            //   }
-            // );
-            // Return the summaries
-            // const summaries = response.data.choices[0].text.trim();
-            const summaries = extractedData;
-            return {
-                Language: languages,
-                url,
-                extractedData,
-                summaries,
-            };
-        }
-        catch (error) {
-            console.error('Error generating summaries:', error);
-            throw error;
-        }
-    });
-}
+// Error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'An error occurred' });
+});
+// Error logging middleware
+app.use((err, req, res, next) => {
+    // Log the error here or use a logging library
+    console.error(err.stack);
+    res.status(500).json({ error: 'An error occurred' });
+});
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
